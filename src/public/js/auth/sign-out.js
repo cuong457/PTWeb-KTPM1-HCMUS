22a2ff56624d5cefac369e1db3390c15667cd301
Phuc Dat <bluehairmylove2014@gmail.com>
2022-12-28 00:14:19 +0700

    fix and change header front-end

--- src/public/js/auth/sign-out.js
+++ src/public/js/auth/sign-out.js
@@ -4,20 +4,17 @@ export const signOut = async (e) => {
 
     if (!response.ok) {
       const errRes = await response.json();
-      console.log(errRes.message);
       alert(errRes.message);
       return;
     }
 
     const data = await response.json();
-    console.log("logout successfully");
     setTimeout(() => {
       // window.location.reload(true);
       alert("logout successfully");
       window.location.replace("/admin/sign-in");
     }, 500);
   } catch (err) {
-    console.log(err.message);
     alert(err.message);
   }
 };
